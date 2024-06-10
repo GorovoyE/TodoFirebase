2ca4faca67059f906e42f0b6cb6ded4eb7a65729
Eugene Gorovoy <kaknazvattebya@gmail.com>
2024-06-10 12:52:18 +0700

    add layers, some dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.firebase)
 }
 
 android {
@@ -45,4 +46,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+//    kapt(libs.androidx.lifecycle.compiler)
 }
--- app/src/main/java/com/gorovoyeg/todofirebase/presentation/MainActivity.kt
+++ app/src/main/java/com/gorovoyeg/todofirebase/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.gorovoyeg.todofirebase
+package com.gorovoyeg.todofirebase.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.gorovoyeg.todofirebase.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
